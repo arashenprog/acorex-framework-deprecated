13da8597cbe8f6d1b5345f4b467bba90f4ddd05b
arashenprog <arash.oshnoudi@gmail.com>
2020-02-03 14:05:27 +0330

    popover animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +24,7 @@ import {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AXTextBoxModule,
     AXButtonModule,
