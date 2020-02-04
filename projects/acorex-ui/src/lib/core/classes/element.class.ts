75a305fdba7903a0651273674a9316fe8edf7a46
asafari72 <asafari72@gmail.com>
2020-02-04 19:12:14 +0330

    change \\ checkbox

--- projects/acorex-ui/src/lib/core/classes/element.class.ts
+++ projects/acorex-ui/src/lib/core/classes/element.class.ts
@@ -2,46 +2,46 @@ import { AXHtmlUtil } from '../utils/utils.api';
 import { Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 
 export abstract class AXBaseComponent {
-    uid: string = AXHtmlUtil.getUID();
-    constructor() {
-    }
+  uid: string = AXHtmlUtil.getUID();
+  constructor() {
+  }
 }
 
 export abstract class AXBaseSizableComponent extends AXBaseComponent {
-    @Input()
-    size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  @Input()
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
 export abstract class AXBaseClickableComponent extends AXBaseSizableComponent {
-    @Output()
-    click: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @Output()
+  click: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
 }
 
 export abstract class AXBaseButtonComponent extends AXBaseClickableComponent {
 
 }
-export abstract class AXCheckedBaseComponent extends AXBaseComponent {
-    @Input() label: string = '';
-    constructor(protected cdr: ChangeDetectorRef) {
-      super();
-    }
-    // Value
-    @Output()
-    valueChange: EventEmitter<boolean> = new EventEmitter<boolean>();
-    //
-    protected _value: boolean = false;
-    //
-    set value(val: boolean) {
-      if (this._value !== val) {
-        this._value = val;
-        this.valueChange.emit(val);
-        this.cdr.markForCheck();
-        this.cdr.detectChanges();
-      }
-    }
-    //
-    @Input()
-    get value(): boolean {
-      return this._value;
+export abstract class AXCheckedBaseComponent extends AXBaseSizableComponent {
+  @Input() label: string = '';
+  constructor(protected cdr: ChangeDetectorRef) {
+    super();
+  }
+  // Value
+  @Output()
+  valueChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  //
+  protected _value: boolean = false;
+  //
+  set value(val: boolean) {
+    if (this._value !== val) {
+      this._value = val;
+      this.valueChange.emit(val);
+      this.cdr.markForCheck();
+      this.cdr.detectChanges();
     }
   }
+  //
+  @Input()
+  get value(): boolean {
+    return this._value;
+  }
+}
